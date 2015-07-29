8954cc04c5e8ee8c7f05970a9758fec52318c62f
jon <blooberr@gmail.com>
2015-07-28 19:12:07 -0700

    indented

--- ruby/btree.rb
+++ ruby/btree.rb
@@ -1,59 +1,58 @@
 class BinaryTree
 	attr_accessor :head
 
-  class Node
-    attr_accessor :payload, :left, :right
+	class Node
+		attr_accessor :payload, :left, :right
 
-    def initialize(payload, left, right)
-      @payload = payload
-      @left = left
-      @right = right
-    end
-  end
+		def initialize(payload, left, right)
+			@payload = payload
+			@left = left
+			@right = right
+		end
+	end
 
 	# insert locates position of next node 
-  def insert(payload)
-
+	def insert(payload)
 		# handle base case if there's nothing
 		if @head.nil?
-		 @head = Node.new(payload, nil, nil)
-		 return
+			@head = Node.new(payload, nil, nil)
+			return
 		end
 
-    current_node = @head
+		current_node = @head
 
-    loop do
-      if payload > current_node.payload
-        if current_node.right.nil?
-          current_node.right = Node.new(payload, nil, nil)
-          break
-        else
-          current_node = current_node.right
-        end
-      else
-        # node payload is less than current_node's payload, so insert to left
-        if current_node.left.nil?
-          current_node.left = Node.new(payload, nil, nil)
-          break
-        else
-          current_node = current_node.left
-        end     
-      end
-    end
-      
+		loop do
+			if payload > current_node.payload
+				if current_node.right.nil?
+					current_node.right = Node.new(payload, nil, nil)
+					break
+				else
+					current_node = current_node.right
+				end
+			else
+				# node payload is less than current_node's payload, so insert to left
+				if current_node.left.nil?
+					current_node.left = Node.new(payload, nil, nil)
+					break
+				else
+					current_node = current_node.left
+				end     
+			end
+		end
+
 	end
 
 	# traverses tree in dfs order
 	def dfs_recur(node, array=[])
 		return if node.nil?
-    dfs_recur(node.left, array)
+		dfs_recur(node.left, array)
 		array << node.payload
 		dfs_recur(node.right, array)
 	end
 
 	def inorder(node)
 		current_node = node
-    traverse = []
+		traverse = []
 
 		sorted = []
 
@@ -79,10 +78,10 @@ class BinaryTree
 		traverse = []
 		traverse << node
 
-    loop do
-      break if traverse.empty?
+		loop do
+			break if traverse.empty?
 			current_node = traverse.last
- 			puts "current_node - #{traverse.pop.payload}"
+			puts "current_node - #{traverse.pop.payload}"
 			traverse << current_node.right if !current_node.right.nil?
 			traverse << current_node.left if !current_node.left.nil?
 		end
--- ruby/sort.rb
+++ ruby/sort.rb
@@ -5,7 +5,7 @@ array = [7, 4, 9, 1, 6, 14, 10]
 bt = BinaryTree.new
 
 array.each do |value|
-  bt.insert(value)
+	bt.insert(value)
 end
 
 @a = []
