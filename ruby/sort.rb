cf62719881001c6122841bef29e063386ee125f7
jon <blooberr@gmail.com>
2015-07-28 19:10:34 -0700

    adding ruby version of binary tree

---
+++ ruby/sort.rb
@@ -0,0 +1,16 @@
+require_relative 'btree.rb'
+
+array = [7, 4, 9, 1, 6, 14, 10]
+
+bt = BinaryTree.new
+
+array.each do |value|
+  bt.insert(value)
+end
+
+@a = []
+bt.dfs_recur(bt.head, @a)
+puts "recur - #{@a.inspect}"
+
+puts "inorder - #{bt.inorder(bt.head)}"
+
